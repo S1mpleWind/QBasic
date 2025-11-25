ce7674195dab7c1a1bee052324ec0a69dd2520c8
IvonJIAO <ivonjiao@gmail.com>
2025-11-25 20:55:20 +0800

    phase1:init-ui

---
+++ ui/mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_cmdLineEdit_editingFinished()
+{
+    QString cmd = ui->cmdLineEdit->text();
+    ui->cmdLineEdit->setText("");
+
+    ui->CodeDisplay->append(cmd);
+}
+
+void MainWindow::on_btnLoadCode_clicked()
+{
+    qDebug()<<"LOAD";
+}
+
+void MainWindow::on_btnRunCode_clicked()
+{
+     qDebug()<<"RUN";
+}
+
+void MainWindow::on_btnClearCode_clicked()
+{
+    qDebug()<<"CLEAR";
+
+    //reset ui
+    ui->CodeDisplay->setText("");
+    //TODO:clear the program structure
+
+}
